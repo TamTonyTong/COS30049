0c970d636504999279273f15ed906fe437053dad
Tâm Tống <tonytutam@gmail.com>
2025-01-17 10:12:19 +0700

    Organize Web Structure

---
+++ src/app/trade/buy/page.tsx
@@ -0,0 +1,9 @@
+import Header from '@/components/header'
+import TradingForm from '@/components/trading-form'
+export default function TradingPage() {
+    return (   
+        <main className="container mx-auto px-4 py-8">
+            <Header />
+        </main>
+    )
+}
